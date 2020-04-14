0c1174c58b0894b6cecec0f3eb25b2ab6d4f3280
mohamedismail3381 <62036749+mohamedismail3381@users.noreply.github.com>
2020-04-13 21:23:23 -0700

    Add Product And Delete Priduct functions With Views

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -8,6 +8,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Emarket.Models;
+using System.IO;
 
 
 namespace Emarket.Controllers
@@ -60,42 +61,94 @@ namespace Emarket.Controllers
             }
             return View(P);
         }
-        //public ActionResult Search(string key)
-        //{
+		[HttpGet]
+		public ActionResult Add()
+		{
+			ViewBag.category_id = new SelectList(db.Categories, "id", "name");
+			return View();
+		}
+		[HttpPost]
+		public ActionResult Add(Product product)
+		{
+			if (product.ImageFile == null)
+			{
+				ViewBag.error = "This is Required";
+			}
+			else
+			{
+				string filename = Path.GetFileNameWithoutExtension(product.ImageFile.FileName);
+				string Extention = Path.GetExtension(product.ImageFile.FileName);
+				filename += Extention;
+				product.image = "~/photos/" + filename;
+				filename = Path.Combine(Server.MapPath("~/photos/"), filename);
+				if (Extention.ToLower() == ".jpg" || Extention.ToLower() == ".png" || Extention.ToLower() == ".jpeg")
+				{
+					if (ModelState.IsValid)
+					{
+						product.ImageFile.SaveAs(filename);
+						db.Products.Add(product);
+						var item2 = db.Categories.Find(product.category_id);
+						item2.number_of_products++;
+						db.Entry(item2).State = EntityState.Modified;
+						db.SaveChanges();
+						return RedirectToAction("Layout");
+					}
+				}
+				else
+				{
+					ViewBag.msg = "Invalid File Type";
+				}
+			}
+			ModelState.Clear();
+			ViewBag.category_id = new SelectList(db.Categories, "id", "name", product.category_id);
+			return View();
+		}
+		public ActionResult Delete(int id)
+		{
+			var item = db.Products.Where(x => x.id == id).First();
+			var item2 = db.Categories.Find(item.category_id);
+			item2.number_of_products--;
+			db.Entry(item2).State = EntityState.Modified;
+			db.Products.Remove(item);
+			db.SaveChanges();
+			return RedirectToAction("Layout");
+		}
+		//public ActionResult Search(string key)
+		//{
 
-        //    IEnumerable<Category> rec;
-        //    foreach (var item in db.Categories)
-        //    {
-        //        if (item.name == key)
-        //        {
-        //             rec = from id in db.Categories
-        //                      select id;
-        //        }
-        //    }
-        //    IEnumerable<Product> se = from p in db.Products
-        //             where p.category_id == rec
-        //             select p;
+		//    IEnumerable<Category> rec;
+		//    foreach (var item in db.Categories)
+		//    {
+		//        if (item.name == key)
+		//        {
+		//             rec = from id in db.Categories
+		//                      select id;
+		//        }
+		//    }
+		//    IEnumerable<Product> se = from p in db.Products
+		//             where p.category_id == rec
+		//             select p;
 
 
-        //    return View();
-        // }
-        //    //}
-        //[ActionName("Layout")]
-        //public ActionResult Search(string key)
-        //{
-        //    //IEnumerable<Product> selection = from p in db.Products
-        //    //                                 where p.Category.name == key
-        //    //                                 select p;
-        //    //IEnumerable<Category>
-        //    var selection = from p in db.Categories
-        //                                      where p.name==key
-        //                                      select new {p.id};
-        //    var bsmallah = from q in db.Products
-        //                                    where selection.Equals(q.id)
-        //                                    select q;
-        //    return View(bsmallah);
-        //}
+		//    return View();
+		// }
+		//    //}
+		//[ActionName("Layout")]
+		//public ActionResult Search(string key)
+		//{
+		//    //IEnumerable<Product> selection = from p in db.Products
+		//    //                                 where p.Category.name == key
+		//    //                                 select p;
+		//    //IEnumerable<Category>
+		//    var selection = from p in db.Categories
+		//                                      where p.name==key
+		//                                      select new {p.id};
+		//    var bsmallah = from q in db.Products
+		//                                    where selection.Equals(q.id)
+		//                                    select q;
+		//    return View(bsmallah);
+		//}
 
 
-    }
+	}
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -12,6 +12,8 @@ namespace Emarket.Models
     using System;
     using System.Collections.Generic;
 	using System.ComponentModel.DataAnnotations;
+	using System.ComponentModel;
+	using System.Web;
 	[MetadataType(typeof(Product))]
 	public partial class Product
     {
@@ -21,10 +23,12 @@ namespace Emarket.Models
 		[Display(Name = "Product Price")]
 		[DisplayFormat(DataFormatString = "{0:C}", ApplyFormatInEditMode = false)]
 		public Nullable<double> price { get; set; }
+		[DisplayName("Upload File")]
         public string image { get; set; }
         public string description { get; set; }
         public Nullable<int> category_id { get; set; }
     
         public virtual Category Category { get; set; }
-    }
+		public HttpPostedFileBase ImageFile { get; set; }
+	}
 }
