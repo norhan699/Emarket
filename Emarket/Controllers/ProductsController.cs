18cfd0554ffe5845831d126c83fbcf714c7abe9f
nada388 <nadahisham388@gmail.com>
2020-05-19 00:37:36 +0200

    Database of phase 2

--- Emarket/Controllers/ProductsController.cs
+++ Emarket/Controllers/ProductsController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -23,65 +25,30 @@ namespace Emarket.Controllers
             return View(products.ToList());
         }
 
-        // GET: Products/Details/5
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
 
-        //public ActionResult Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Product product = db.Products.Find(id);
-        //    if (product == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(product);
-        //}
-
-        // GET: Products/Create
-
-        //public ActionResult Create()
-        //{
-        //    ViewBag.category_id = new SelectList(db.Categories, "id", "name");
-        //    return View();
-        //}
-
-        // POST: Products/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "id,name,price,image,description,category_id")] Product product)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Products.Add(product);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    ViewBag.category_id = new SelectList(db.Categories, "id", "name", product.category_id);
-        //    return View(product);
-        //}
-
-
-        //protected override void Dispose(bool disposing)
-        //{
-        //    if (disposing)
-        //    {
-        //        db.Dispose();
-        //    }
-        //    base.Dispose(disposing);
-        //}
         public ActionResult Layout()
         {
+            EmarketDBEntities emarketDB = new EmarketDBEntities();
+            var categorylist = emarketDB.Categories.ToList();
+            SelectList list = new SelectList(categorylist, "id", "name");
+            ViewBag.CategoryList = list;
+
             var record = db.Products.ToList();
             return View(record);
         }
         public ActionResult Search(string key)
         {
+            EmarketDBEntities emarketDB = new EmarketDBEntities();
+            var categorylist = emarketDB.Categories.ToList();
+            SelectList list = new SelectList(categorylist, "id", "name");
+            ViewBag.CategoryList = list;
+
+
             //var categoryID = (from p in db.Categories
             //                  where p.name == key
             //                  select new Category { id = p.id });
@@ -96,6 +63,123 @@ namespace Emarket.Controllers
             //  var listOfProducts = db.Products.Where(x => x.id == parsed).ToList();
             return View(listOfProducts);
         }
+        public ActionResult MoreInfo(int id)
+        {
+            Product P = new Product();
+
+            using (db)
+            {
+				P = db.Products.Include(x => x.Category).SingleOrDefault(p => p.id == id);
+            }
+            return View(P);
+        }
+
+        // GET: Productscopy/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.category_id = new SelectList(db.Categories, "id", "name", product.category_id);
+            return View(product);
+        }
+
+        // POST: Productscopy/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,name,price,image,description,category_id")] Product product,HttpPostedFileBase ImageFile)
+        {
+
+  
+                    if (ModelState.IsValid)
+                    {
+                        string path = "";
+                        if (ImageFile.FileName.Length > 0)
+                        {
+                            path = "~/Photos/" + Path.GetFileName(ImageFile.FileName);
+                            ImageFile.SaveAs(Server.MapPath(path));
+                        }
+                        product.image = path;
+
+                        db.Entry(product).State = EntityState.Modified;
+                        db.SaveChanges();
+                        return RedirectToAction("Layout");
+                    }
+                
+
+            
+
+            ViewBag.category_id = new SelectList(db.Categories, "id", "name", product.category_id);
+            return View(product);
+        }
+       
+        [HttpGet]
+        public ActionResult Add()
+        {
+            ViewBag.category_id = new SelectList(db.Categories, "id", "name");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Product product)
+        {
+            if (product.ImageFile == null)
+            {
+                ViewBag.error = "This is Required";
+            }
+            else
+            {
+                string filename = Path.GetFileNameWithoutExtension(product.ImageFile.FileName);
+                string Extention = Path.GetExtension(product.ImageFile.FileName);
+                filename += Extention;
+                product.image = "~/photos/" + filename;
+                filename = Path.Combine(Server.MapPath("~/photos/"), filename);
+                if (Extention.ToLower() == ".jpg" || Extention.ToLower() == ".png" || Extention.ToLower() == ".jpeg")
+                {
+                    if (ModelState.IsValid)
+                    {
+                        product.ImageFile.SaveAs(filename);
+                        db.Products.Add(product);
+                        var item2 = db.Categories.Find(product.category_id);
+                        item2.number_of_products++;
+                        db.Entry(item2).State = EntityState.Modified;
+                        db.SaveChanges();
+                        return RedirectToAction("Layout");
+                    }
+                }
+                else
+                {
+                    ViewBag.msg = "Invalid File Type";
+                }
+            }
+            ModelState.Clear();
+            ViewBag.category_id = new SelectList(db.Categories, "id", "name", product.category_id);
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            var item = db.Products.Where(x => x.id == id).First();
+            var item2 = db.Categories.Find(item.category_id);
+            item2.number_of_products--;
+            db.Entry(item2).State = EntityState.Modified;
+            db.Products.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Layout");
+        }
+    }
+
+}
+
+        
+
+ //   ------   ---------------------------------------------------------------------------------------------------
         //public ActionResult Search(string key)
         //{
 
@@ -133,5 +217,3 @@ namespace Emarket.Controllers
         //}
 
 
-    }
-}
